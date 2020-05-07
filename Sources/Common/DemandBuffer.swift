b1781ac35f5668decb0c59275cad55aeae9d85c5
Eric Blachère <eric.blachere.n7@gmail.com>
2020-05-07 16:39:55 +0200

    The package is now compatible iOS 10 / MacOS 10_12 / tvOS v10 / WatchOS v3
    Even if the package is compatible, it doesn’t mean RxCombine is available for these targets. However you can import the module and use it in your app components annotated with @available(iOS 13, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
    The goal is for developers to be able to develop new features for iOS 13 while still maintaining a target of iOS 12 or below.

--- Sources/Common/DemandBuffer.swift
+++ Sources/Common/DemandBuffer.swift
@@ -18,6 +18,7 @@ import class Foundation.NSRecursiveLock
 /// In a sense, the subscription only relays the requests for demand, as well
 /// the events emitted by the upstream — to this buffer, which manages
 /// the entire behavior and backpressure contract
+@available(iOS 13, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 class DemandBuffer<S: Subscriber> {
     private let lock = NSRecursiveLock()
     private var buffer = [S.Input]()
@@ -115,6 +116,7 @@ class DemandBuffer<S: Subscriber> {
 }
 
 // MARK: - Private Helpers
+@available(iOS 13, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 private extension DemandBuffer {
     /// A model that tracks the downstream's
     /// accumulated demand state
@@ -126,6 +128,7 @@ private extension DemandBuffer {
 }
 
 // MARK: - Internally-scoped helpers
+@available(iOS 13, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Subscription {
     /// Reqeust demand if it's not empty
     ///
@@ -136,6 +139,7 @@ extension Subscription {
     }
 }
 
+@available(iOS 13, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension Optional where Wrapped == Subscription {
     /// Cancel the Optional subscription and nullify it
     mutating func kill() {
